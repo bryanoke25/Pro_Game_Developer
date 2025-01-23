0723277105cbf8d3a985e14f29f27599c9a02c58
bryanoke25 <bryan.oke25@gmail.com>
2025-01-23 18:30:14 +0000

    Add files via upload

--- controling_character_using_key.py
+++ controling_character_using_key.py
@@ -1,22 +1,29 @@
 import pygame
-from time import *
 from pygame.locals import *
+import time
+import random
 
 pygame.init()
+
 WIDTH = 600
 HEIGHT = 600
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 player_x = 200
 player_y = 200
-keys = [False, False, False, False]
+
+#        Up    Left   Down   Right
+Keys = [False, False, False, False]
 
 player = pygame.image.load("C:/Users/bryan/OneDrive/Desktop/JetLearn/Pro_Game_Developer/images/character.png")
-background = pygame.image.load("C:/Users/bryan/OneDrive/Desktop/JetLearn/Pro_Game_Developer/images/background.png")
+
+bg = pygame.image.load("C:/Users/bryan/OneDrive/Desktop/JetLearn/Pro_Game_Developer/images/background.png")
 
 while player_y < 600:
-    screen.blit(background, (0, 0))
+    screen.blit(bg, (0, 0))
     screen.blit(player, (player_x, player_y))
+
     pygame.display.flip()
 
     for event in pygame.event.get():
@@ -26,46 +33,47 @@ while player_y < 600:
 
         #check if any keyboard button is pressed
         if event.type == pygame.KEYDOWN:
-            if event.key == K_UP:
-                keys[0] = True
-            elif event.key == K_LEFT:
-                keys[1] = True
-            elif event.key == K_DOWN:
-                keys[2] = True
-            elif event.key == K_RIGHT:
-                keys[3] == True
-
+            if event.key == K_w:
+                Keys[0] = True
+            if event.key == K_a:
+                Keys[1] = True
+            if event.key == K_s:
+                Keys[2] = True
+            if event.key == K_d:
+                Keys[3] = True
+        
         #check if any keyboard button is released
         if event.type == pygame.KEYUP:
-            if event.key == K_UP:
-                keys[0] = False
-            elif event.key == K_LEFT:
-                keys[1] = False
-            elif event.key == K_DOWN:
-                keys[2] = False
-            elif event.key == K_RIGHT:
-                keys[3] == False
+            if event.key == K_w:
+                Keys[0] = False
+            if event.key == K_a:
+                Keys[1] = False
+            if event.key == K_s:
+                Keys[2] = False
+            if event.key == K_d:
+                Keys[3] = False
 
-    if keys[0]:
+    #adding movement
+    if Keys[0]:
         if player_y > 0:
-            player_y -= 4
-
-    elif keys[2]:
+            player_y -= 70
+    
+    if Keys[2]:
         if player_y < 536:
-            player_y += 4
-
-    if keys[1]:
+            player_y += 70
+    
+    if Keys[1]:
         if player_x > 0:
-            player_x  -= 2
-
-    elif keys[3]:
+            player_x -= 50
+    
+    if Keys[3]:
         if player_x < 536:
-            player_x += 2
+            player_x += 50
+    
+    player_y += 70 #gravity
+    time.sleep(0.05)
 
-    player_y += 3
-    sleep(0.05)
-
-print("GAME OVER")
-
+print("Game Over! :(")
+    
 
 
